34a499ee2c3edf7a7357fdde2ece7fc6c268c4ac
Rowley Thompson <thompsonrjwt@gmail.com>
2025-12-06 11:38:43 +0000

    4000m

--- src/components/BackgroundVideo/BackgroundVideo.jsx
+++ src/components/BackgroundVideo/BackgroundVideo.jsx
@@ -19,7 +19,7 @@ export default function BackgroundVideo ({ onVideoLoaded }) {
         if (videoRef.current) {
           videoRef.current.play();
         }
-      }, 7000);
+      }, 4000);
 
       return () => clearTimeout(timer);
     }
